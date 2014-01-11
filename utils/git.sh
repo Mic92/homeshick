Mic92/homeshick

commit e8b089859335d1f2bc7c7b99477008a4bb57af10

Add . (dot) to github shorthand syntax Also remove underscore from the username part

utils/git.sh
@@ -14,7 +14,7 @@ function clone {
 	[[ ! $1 ]] && help_err clone
 	local git_repo=$1
 	local repo_path="$repos/$(parse_url $git_repo)"
-	if [[ $git_repo =~ ^([0-9A-Za-z_-]+\/[0-9A-Za-z_-]+)$ ]]; then
+	if [[ $git_repo =~ ^([0-9A-Za-z-]+/[0-9A-Za-z_-\.]+)$ ]]; then
 		git_repo="https://github.com/$git_repo.git"
 	fi
 	pending 'clone' $git_repo

commit 689f0287f6474185b61c38e620fc4b6e672b7f76

e8b08985 introduced a bug with github shorthand The dash has to be at the end of the regex, _-\. is parsed as a range.

utils/git.sh
@@ -17,7 +17,7 @@ function git_shorthand {
 		printf -- "$1"
 		return
 	fi
-	if [[ $1 =~ ^([0-9A-Za-z-]+/[0-9A-Za-z_-\.]+)$ ]]; then
+	if [[ $1 =~ ^([0-9A-Za-z-]+/[0-9A-Za-z_\.-]+)$ ]]; then
 		printf -- "https://github.com/$1.git"
 		return
 	fi